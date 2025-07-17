f0545cf20ec9627c07e9ffffb60e391370101052
konstalbristers <kcparis33@gmail.com>
2025-07-17 07:34:55 -0700

    wrote third function that makes a form and inputs your information as variables

--- ContactForm.php
+++ ContactForm.php
@@ -38,6 +38,21 @@ function validateEmail($data, $fieldName) {
     }// makes $retval = $data filtered amd checks if $retval is a valid e-mail
 return($retval);
 }
+function displayForm($Sender, $Email, $Subject, $Message) {
+    ?> <h2 style="text-align:center">Contact Me</h2>
+    <form name="contact" action="ContactForm.php" method="post">
+        <p>Your Name:
+            <input type="text" name="Sender" value="<?php echo $Sender; ?>" /></p>
+        <p>Your E-mail:
+            <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
+        <p>Subject:
+            <input type="text" name="Subject" value="<?php echo $Subject; ?>" /></p>
+        <p>Message:
+            <textarea name="Message"><?php echo $Message; ?></textarea></p>
+        <p><input type="reset" value="Clear Form" />&nbsp: &nbsp;
+            <input type="submit" name="Submit" value="Send Form" /> </p>
+    </form>
+ <?php } // function makes a form and inputs your information as variables 
 ?>
 </body>
 </html>
