f9003acd7ba9d5001e6dbd8943ae2926ecdeb6ae
konstalbristers <kcparis33@gmail.com>
2025-07-16 23:00:39 -0700

    Wrote first function (validateInput()) that checks if $data is empty, reminds you to fill $fieldname, increments $errorCount and otherwise makes $retval = $data without whitespace and backlashes

---
+++ ContactForm.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ContactForm</title>
+</head>
+<body>
+<?php
+function validateInput($data, $fieldName) {
+    global $errorCount;
+    if (empty($data)) {
+        echo "\"$fieldName\" is a requied field.<br />\n";
+        ++$errorCount;
+        $retval = "";
+    } // checks if data is empty, and if it is empty it reminds you to fill it out and increments $errorCount
+    else {
+        $retval = trim($data);
+        $retval = stripslashes($retval);
+    } // $retval is $data without whitespace and backslashes
+
+    return($retval);
+}
+?>
+</body>
+</html>
